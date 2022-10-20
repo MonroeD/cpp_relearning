68a6eb0f4916cd1f0a7fc482754ff0e243cc8201
haohao <haohao@example.com>
2022-10-20 17:25:17 +0800

    add itemrm5 iterm6

---
+++ EffectiveModerenC++/2_auto/iterm5_prefer_auto_to_explicit_type_declarations.cc
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <iostream>
+
+int main(void) {
+  {
+    // (1) auto 避免初始化
+    int x; // 未初始化
+    //auto y; // 会报错
+    auto y = 0;
+  }
+
+  {
+#if 0
+    // (2) auto 省略冗长的声明类型
+    ...
+    typename std::iterator<It>::value_type t = b;
+    auto t = b;
+    ...
+#endif
+  }
+
+  {
+#if 0
+    // (3) 直接产生闭包，lambda表达式
+    auto f = [] {
+    };
+#endif
+  }
+
+  {
+    // (4) 类型依赖问题
+    std::vector<int> v;
+    unsigned sz = v.size();
+    // 在 windows-32bit std::vector<int>::size_type and unsigned -> 32bit
+    // 在 windows-64bit std::vector<int>::size_type 是64 bit , unsigned -> 32bit
+    
+    // 因为用auto解决更好
+    auto szz = v.size();
+  }
+
+  {
+    // TODO
+    std::unordered_map<std::string, int> m = {{"ni", 1}, {"hao", 2}};
+    //for (std::pair<std::string, int>& p : m) {
+    for (auto& p : m) {
+      p.second = 100;
+    }
+    for (const std::pair<std::string, int>& p : m) {
+      std::cout << p.first << " : " << p.second << "\n";
+    }
+  }
+
+  // 最后，使用auto是建议，不是必须
+
+  return 0;
+}
